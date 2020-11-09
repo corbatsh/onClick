5ee6e341500a3dc2ba4f62f0f83e4fe9e7ac415e
Kamil <60507310+corbatsh@users.noreply.github.com>
2020-11-09 15:00:31 +0100

    sage all changes

--- script.js
+++ script.js
@@ -8,10 +8,16 @@ const wierszykiChild = document.createElement('div');
 wierszykiChild.classList.add('wierszykiChild');
 wierszyki.append(wierszykiChild);
 
+const wierszykiChildTag = document.createElement('div');
+wierszykiChildTag.classList.add('wierszykiChildTag');
+wierszyki.append(wierszykiChildTag);
+
 const maindiv = document.createElement('div');
 maindiv.classList.add('maindiv');
 maindiv.append(childDiv, wierszyki);
 document.body.append(maindiv);
+
+
 
 // tworzenie elementu ze zdjeciem i dodanie go do childDiva
 const createImgBlock = (imgSrc, alt) => {
@@ -26,16 +32,23 @@ createImgBlock('pitbull.jpg', 'Pitbull');
 createImgBlock('buldog.jpg', 'Buldog francuski');
 createImgBlock('mops.jpg', 'Mops');
 createImgBlock('corgi.jpg', 'Corgi');
+createImgBlock('samoyed.jpg', 'Samoyed');
+createImgBlock('husky.jpg', 'Husky');
+createImgBlock('chowchow.jpg', 'Chow chow');
+createImgBlock('owczarek.jpg', 'Owczarek niemiecki');
 
 // Pobranie wszystkich blockow ze zdjeciami
 const textBlocks = document.querySelectorAll('.textblock');
-
+wierszykiChildTag.innerText = `Opis psa`;
+        wierszykiChild.innerText = `Nazwa psa`;
 // dodanie ewentu onClick do wszystkich textBlocków
 textBlocks.forEach(element => {
     element.addEventListener('click', (event) => {
         // usuwanie klasy 'active' wszystkich elementów które mają klasę textblock i active
         document.querySelectorAll('.textblock.active').forEach(element => {
             element.classList.remove('active');
+             const dogName = event.target.children[0].alt;
+        wierszykiChildTag.innerHTML = dogName;
         });
 
         // rodzic elementu który został nacisnięty dostaje klase 'active'
@@ -45,26 +58,73 @@ textBlocks.forEach(element => {
         const dogName = event.target.children[0].alt;
         wierszykiChild.innerHTML = dogName;
 
+        const dogDesc1 = ` to muskularny, korpulentny pies,
+        który osiąga do 53 cm wysokości w kłębie oraz wagę do 27 kg, 
+        W oczy rzuca się muskularna głowa z półstojącymi uszami`;
+
+        const dogDesc2 = ` jedna z ras psów, należąca do grupy psów do towarzystwa,
+        zaklasyfikowana do sekcji małych psów molosowatych. Typ dogowaty.`;
+
+        const dogDesc3 = ` rasa psa należąca do grupy psów do towarzystwa,
+        zaklasyfikowana do sekcji małych psów molosowatych. Typ dogowaty`;
+
+        const dogDesc4 = ` jedna z ras psów, należąca do grupy psów pasterskich i zaganiających,
+        zaklasyfikowana do sekcji psów pasterskich. Typ jamnikowaty`;
+
+        const dogDesc5 = ` rasa psów, należąca do grupy szpiców i psów pierwotnych,
+        zaklasyfikowana do sekcji północnych psów zaprzęgowych`;
+
+        const dogDesc6 = ` jedna z ras psów, należąca do grupy szpiców i psów w typie pierwotnym,
+        zaklasyfikowana do sekcji północnych psów zaprzęgowych`;
+
+        const dogDesc7 = ` rasa psa należąca do grupy szpiców i psów pierwotnych,
+                 zaklasyfikowana do sekcji szpiców azjatyckich i ras pokrewnych. Typ lisowaty`;
+        
+        const dogDesc8 = ` jedna z ras psów należąca do grupy psów pasterskich,
+         sklasyfikowana do sekcji psów pasterskich.
+        Według klasyfikacji FCI podlega próbom pracy`;
+
+        
         // dopisywanie opisu
         switch(dogName) {
             case 'Pitbull':
-                wierszykiChild.innerHTML += ` - to muskularny, korpulentny pies,
-                 który osiąga do 53 cm wysokości w kłębie oraz wagę do 27 kg. W oczy rzuca się muskularna głowa z półstojącymi uszami`;
+                wierszykiChildTag.innerText = dogDesc1;
+                 wierszykiChild.innerText = dogName;
                 break;
 
             case 'Buldog francuski':
-                wierszykiChild.innerHTML += ` - jedna z ras psów, należąca do grupy psów do towarzystwa,
-                    zaklasyfikowana do sekcji małych psów molosowatych. Typ dogowaty.`;
+                wierszykiChildTag.innerText = dogDesc2;
+                 wierszykiChild.innerText = dogName;
                 break;
             case 'Mops': 
-                wierszykiChild.innerHTML += ` - rasa psa należąca do grupy psów do towarzystwa,
-                 zaklasyfikowana do sekcji małych psów molosowatych. Typ dogowaty`;
-                break    
+            wierszykiChildTag.innerText = dogDesc3;
+                 wierszykiChild.innerText = dogName;
+                break; 
 
             case 'Corgi':
-                wierszykiChild.innerHTML += ` - jedna z ras psów, należąca do grupy psów pasterskich i zaganiających,
-                    zaklasyfikowana do sekcji psów pasterskich. Typ jamnikowaty`;
+                wierszykiChildTag.innerText = dogDesc4;
+                 wierszykiChild.innerText = dogName;
+                break;    
+
+                case 'Samoyed':
+                    wierszykiChildTag.innerText = dogDesc5;
+                 wierszykiChild.innerText = dogName;
+                break;
+
+            case 'Husky':
+                wierszykiChildTag.innerText = dogDesc6;
+                 wierszykiChild.innerText = dogName;
+                break;
+            case 'Chow chow': 
+            wierszykiChildTag.innerText = dogDesc7;
+                 wierszykiChild.innerText = dogName;
+                break;
+
+            case 'Owczarek niemiecki':
+                wierszykiChildTag.innerText = dogDesc8;
+                 wierszykiChild.innerText = dogName;
                 break;    
         }
+        
     });
 })
